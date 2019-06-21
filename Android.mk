18a33512203123f3ec40cfa5a7dfdb23872db10a
Tet35 <ttnani555@gmail.com>
2019-06-21 20:56:05 +0200

    Update GNSS stack and configs

--- Android.mk
+++ Android.mk
@@ -33,9 +33,8 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := nubia
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libloc_api_v02.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libloc_api_v02.so
-LOCAL_MULTILIB := both
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_api_v02.so
+LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
